67288be1a76d6e34a3676aad3a1a66f0fe5a6eae
Diiegohdez <Diiego.mtel@gmail.com>
2022-04-06 21:51:14 -0500

    lista de usuario

--- src/pages/UserList/UserList.jsx
+++ src/pages/UserList/UserList.jsx
@@ -1,41 +1,81 @@
 import React from 'react'
 import "./userList.css"
 import { DataGrid } from '@material-ui/data-grid'
+import {DeleteForever} from '@material-ui/icons'
 
 export default function UserList() {
 
   const columns = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'firstName', headerName: 'First name', width: 130 },
-    { field: 'lastName', headerName: 'Last name', width: 130 },
+    { field: 'id', headerName: 'ID', width: 90 },
+    { field: "user", headerName: 'Nombre de Usuario', width: 210, renderCell: (params)=>{
+      return(
+        <div className="UserListAvatar">
+          <img className="UserListImg"  src={params.row.avatar} alt=""/>
+          {params.row.userName}
+        </div>
+      )
+    } },
+    { field: 'email', headerName: 'Email', width: 180 },
     {
-      field: 'age',
-      headerName: 'Age',
-      type: 'number',
-      width: 90,
+      field: 'status',
+      headerName: 'Estado',
+      width: 120,
     },
     {
-      field: 'fullName',
-      headerName: 'Full name',
-      description: 'This column has a value getter and is not sortable.',
-      sortable: false,
-      width: 160,}
+      field: 'transaccion',
+      headerName: 'Transaccion Volumen',
+      width: 190,
+    },
+    {
+      field: 'action',
+      headerName: 'Accion',
+      width: 150,
+      renderCell: (params)=>{
+        return(
+          <>
+           <button className="UserListModif">Editar</button>
+           <DeleteForever className="UserListIco"/>
+          </>
+        )
+      }
+    },
   ];
   
   const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 1, userName: 'Juan Zapata', avatar:'https://images.pexels.com/photos/810775/pexels-photo-810775.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    email:'JuanZ@gmail.com', status:'Active', transaccion:'$150.000' },
+
+    { id: 2, userName: 'Juan Zapata', avatar:'https://images.pexels.com/photos/810775/pexels-photo-810775.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    email:'JuanZ@gmail.com', status:'Active', transaccion:'$150.000' },
+
+    { id: 3, userName: 'Juan Zapata', avatar:'https://images.pexels.com/photos/810775/pexels-photo-810775.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    email:'JuanZ@gmail.com', status:'Active', transaccion:'$150.000' },
+
+    { id: 4, userName: 'Juan Zapata', avatar:'https://images.pexels.com/photos/810775/pexels-photo-810775.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    email:'JuanZ@gmail.com', status:'Active', transaccion:'$150.000' },
+
+    { id: 5, userName: 'Juan Zapata', avatar:'https://images.pexels.com/photos/810775/pexels-photo-810775.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    email:'JuanZ@gmail.com', status:'Active', transaccion:'$150.000' },
+
+    { id: 6, userName: 'Juan Zapata', avatar:'https://images.pexels.com/photos/810775/pexels-photo-810775.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    email:'JuanZ@gmail.com', status:'Active', transaccion:'$150.000' },
+
+    { id: 7, userName: 'Juan Zapata', avatar:'https://images.pexels.com/photos/810775/pexels-photo-810775.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    email:'JuanZ@gmail.com', status:'Active', transaccion:'$150.000' },
+
+    { id: 8, userName: 'Juan Zapata', avatar:'https://images.pexels.com/photos/810775/pexels-photo-810775.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    email:'JuanZ@gmail.com', status:'Active', transaccion:'$150.000' },
+
+    { id: 9, userName: 'Juan Zapata', avatar:'https://images.pexels.com/photos/810775/pexels-photo-810775.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    email:'JuanZ@gmail.com', status:'Active', transaccion:'$150.000' },
+
+    { id: 10, userName: 'Juan Zapata', avatar:'https://images.pexels.com/photos/810775/pexels-photo-810775.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    email:'JuanZ@gmail.com', status:'Active', transaccion:'$150.000' },
+
   ];
   return (
     <div className="Userlist">
-        <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection/>
+        <DataGrid rows={rows} disableSelectionOnClick columns={columns} pageSize={9} checkboxSelection/>
     </div>
   )
 }
